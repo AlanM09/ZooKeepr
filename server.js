38a11c97b5c94a6deff1ff7b7ea6461400874a50
Your Name <alanmayer@email.arizona.edu>
2020-12-14 13:30:23 -0800

    create animals and get animals

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -116,6 +117,14 @@ app.post('/api/animals', (req, res) => {
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
   });
 
 app.listen(PORT, () => {
